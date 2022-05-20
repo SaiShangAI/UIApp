6c1fd632b7c74cef7c25b6440cbc1a74776b9605
SaiShang <saishang521@gmail.com>
2022-05-20 23:41:12 +0800

    combine three.js and react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './RA.png';
 import './App.css';
 import fetchAPI from './APICall';
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Stars } from '@react-three/drei'
 
+
+function Box() {
+  return (
+    <mesh><boxBufferGeometry attach='geometry'></boxBufferGeometry>
+      <meshLambertMaterial attach='material' color='hotpink'>
+      </meshLambertMaterial></mesh>
+  )
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hi！Welcome to RA world :)
@@ -14,6 +25,14 @@ function App() {
         >
           Get VM temperature
         </button>
+        <Canvas >
+          <OrbitControls>
+          </OrbitControls>
+          <Stars></Stars>
+          <ambientLight intensity={0.3}>
+          </ambientLight>
+          <Box></Box>
+        </Canvas>
       </header>
     </div>
   );
